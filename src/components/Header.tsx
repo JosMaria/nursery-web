15ef70953806ef9aa56f0e503228aaabacf202ed
JosMaria <josmariaguilar@gmail.com>
2024-06-18 12:26:42 -0400

    refactor code navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,29 @@
-import { Title } from '@nursery/styles'
 import { NavLink } from 'react-router-dom'
+import { Title } from '@nursery/styles'
 
 export const Header = () => {
 	return (
-		<header className='flex justify-between items-center max-md:flex-wrap md:p-1 gap-1'>
+		<header className='flex justify-between items-center max-md:flex-wrap pt-1 md:p-1 gap-1'>
 			<Title className='max-md:mx-1'>Mi Proyecto</Title>
-			<nav className='max-md:order-1 flex w-full md:max-w-lg  text-xs sm:text-sm md:text-base'>
-				<NavLink className={({ isActive }) => `${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 text-center font-medium tracking-wide p-2 hover:bg-nursery-dark-hover focus:outline-none focus:bg-nursery-dark-hover active:opacity-85 border-b-2 border-transparent`} to='/'>Catalogo</NavLink>
-				<NavLink className={({ isActive }) => `${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 text-center font-medium tracking-wide p-2 hover:bg-nursery-dark-hover focus:outline-none focus:bg-nursery-dark-hover active:opacity-85 border-b-2 border-transparent`} to='repertory'>Listado</NavLink>
-				<NavLink className={({ isActive }) => `${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 text-center font-medium tracking-wide p-2 hover:bg-nursery-dark-hover focus:outline-none focus:bg-nursery-dark-hover active:opacity-85 border-b-2 border-transparent`} to='news'>Novedades</NavLink>
-				{/* <a href='#' className='flex-1 hover:bg-emerald-900 border-b-2 border-transparent hover:border-emerald-50 p-1.5'>Novedades</a> */}
+			<nav className='max-md:order-1 flex w-full md:max-w-lg text-xs sm:text-sm md:text-base text-center font-medium tracking-wide'>
+				<NavLink
+					className={({ isActive }) => `navbar ${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 p-1.5 border-b-2 border-transparent`}
+					to='/'
+				>
+					Catalogo
+				</NavLink>
+				<NavLink
+					className={({ isActive }) => `navbar ${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 p-1.5 border-b-2 border-transparent`}
+					to='repertory'
+				>
+					Listado
+				</NavLink>
+				<NavLink 
+					className={({ isActive }) => `navbar ${isActive && 'border-b-nursery-light bg-nursery-dark-hover'} flex-1 p-1.5 border-b-2 border-transparent`} 
+					to='news'
+				>
+					Novedades
+				</NavLink>
 			</nav>
 			<NavLink className={({ isActive }) => `button max-md:mx-1 p-1 rounded-full ${isActive && 'bg-nursery-dark-hover'}`} to='login'>
 				<svg className='h-5 sm:h-6 w-5 sm:w-6 text-nursery-light' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
@@ -17,5 +31,5 @@ export const Header = () => {
 				</svg>
 			</NavLink>
 		</header>
-	)
-}
+	);
+};
