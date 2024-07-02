9da338c6cc5b8045e85119c8890ae316a8d71fea
JosMaria <josmariaguilar@gmail.com>
2024-07-01 23:21:12 -0400

    implementing searcher

--- src/pages/Repertory/Page.tsx
+++ src/pages/Repertory/Page.tsx
@@ -1,29 +1,54 @@
-import { InputText, Title } from '@nursery/styles';
+import { ItemList, Paused, Searcher } from './components';
+import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { Loader } from '@nursery/components';
 import { fetchAllItems } from './service';
-import { ItemList, Paused, Searcher } from './components';
-import { useSearchParams } from 'react-router-dom';
+import { Title } from '@nursery/styles';
 import { useRef } from 'react';
 
 export const RepertoryPage = () => {
-  const { data: itemsFetched, status, isPaused } = useQuery({
+  const [searchParams, setSearchParams] = useSearchParams({ q: '' });
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { data: itemsObtained, status, isPaused } = useQuery({
     queryKey: ['items'],
     queryFn: fetchAllItems
   });
 
+  const search = () => {
+    if (inputRef.current) {
+      const valueToSearch = inputRef.current.value;
+      setSearchParams({ q: valueToSearch });
+    }
+  };
+
+  const clean = () => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+      setSearchParams({ q: '' });
+    }
+  };
+
   if (isPaused) return <Paused />;
 
   return (
-    <div className='flex justify-center m-2'>
+    <div className='flex justify-center m-1'>
       {status === 'pending' && <Loader />}
       {status === 'success' && (
-        <section className='flex flex-col gap-2 max-w-2xl w-full'>
+        <section className='flex flex-col gap-1 max-w-2xl w-full'>
           <div className='flex flex-wrap justify-between'>
             <Title>Listado</Title>
-            <Searcher isEnabled={itemsFetched.length > 0} />
+            <Searcher
+              isEnabled={itemsObtained.length > 0}
+              inputRef={inputRef}
+              search={search}
+              clean={clean}
+            />
           </div>
-          <ItemList items={itemsFetched} />
+          <ItemList 
+            itemsObtained={itemsObtained} 
+            textToFilter={searchParams.get('q') ?? ''} 
+          />
         </section>
       )}
       {status === 'error' && (<p>hubo un error</p>)}
--- src/pages/Repertory/components/ItemList.tsx
+++ src/pages/Repertory/components/ItemList.tsx
@@ -1,36 +1,51 @@
 type ItemListProps = {
-  items: AnswerItemType[];
+  itemsObtained: AnswerItemType[];
+  textToFilter: string;
 };
 
-export const ItemList = ({ items }: ItemListProps) => (
+export const ItemList = ({ itemsObtained, textToFilter }: ItemListProps) => (
   <>
-    {items.length === 0 ? (
+    {itemsObtained.length === 0 ? (
       <p className='max-sm:text-sm w-full text-center font-medium p-2 sm:p-3 bg-nursery-medium border-2 border-nursery-dark'>
         No tenemos ejemplares de plantas para mostrar en este momento. Por favor, vuelve m&aacute;s tarde.
       </p>
-    ) : (
-      <article className='overflow-x-auto h-screen overflow-y-auto'>
-        <table className='min-w-[28rem] w-full whitespace-nowrap'>
-          <thead className='sticky top-0 bg-nursery-dark text-nursery-light whitespace-nowrap text-sm max-sm:text-xs'>
-            <tr className='tracking-wide'>
-              <th className='py-2'>N°</th>
-              <th className='py-2'>Nombre Com&uacute;n</th>
-              <th className='py-2'>Nombre Cientifico</th>
-              <th className='py-2'>Familia</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => (
-              <tr key={index} className='text-sm max-sm:text-xs odd:bg-slate-100 even:bg-slate-50 text-center'>
-                <td className='p-1.5'>{index + 1}</td>
-                <td className='p-1.5'>{item.commonName}</td>
-                <td className='p-1.5 italic'>{item.scientificName}</td>
-                <td className='p-1.5'>{item.family}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </article>
-    )}
+    ) : <Table itemsObtained={itemsObtained} textToFilter={textToFilter} />}
   </>
 );
+
+const Table = ({ itemsObtained, textToFilter }: ItemListProps) => {
+  const itemsFiltered = itemsObtained.filter(item => item.commonName.toLowerCase().includes(textToFilter.toLowerCase()));
+
+  return (
+    <>
+      {itemsFiltered.length === 0 ? (
+        <p className='max-sm:text-sm w-full text-center font-medium p-2 sm:p-3 bg-nursery-medium border-2 border-nursery-dark'>
+          No se encontro ninguna planta con el filtro <b>{textToFilter}</b>
+        </p>
+      ) : (
+        <article className='overflow-x-auto h-screen overflow-y-auto'>
+          <table className='min-w-[28rem] w-full whitespace-nowrap'>
+            <thead className='sticky top-0 bg-nursery-dark text-nursery-light whitespace-nowrap text-sm max-sm:text-xs'>
+              <tr className='tracking-wide'>
+                <th className='py-2'>N°</th>
+                <th className='py-2'>Nombre Com&uacute;n</th>
+                <th className='py-2'>Nombre Cientifico</th>
+                <th className='py-2'>Familia</th>
+              </tr>
+            </thead>
+            <tbody>
+              {itemsFiltered.map((item, index) => (
+                <tr key={index} className='text-sm max-sm:text-xs odd:bg-slate-100 even:bg-slate-50 text-center'>
+                  <td className='p-1.5'>{index + 1}</td>
+                  <td className='p-1.5'>{item.commonName}</td>
+                  <td className='p-1.5 italic'>{item.scientificName}</td>
+                  <td className='p-1.5'>{item.family}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </article>
+      )}
+    </>
+  );
+}
