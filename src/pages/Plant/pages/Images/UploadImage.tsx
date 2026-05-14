54a9e20124b489c563cc73fbbcd67b10f7cc9c07
Jose Maria <josmariaguilar@gmail.com>
2026-05-14 05:12:11 -0400

    FE-025: Dragging and drop files

--- src/pages/Plant/pages/Images/UploadImage.tsx
+++ src/pages/Plant/pages/Images/UploadImage.tsx
@@ -8,44 +8,40 @@ import styles from './scss/UploadImage.module.scss';
 
 export const UploadImage = () => {
 	const [files, setFiles] = useState<File[]>([]);
-	const [isDraggingOver, setIsDraggingOver] = useState(false);
+	const [isDragging, setIsDragging] = useState(false);
 
 	const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
-		setIsDraggingOver(true);
+		setIsDragging(true);
 	}
 
-	const handleDragAOver = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-	};
-	
 	const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault()
-		setIsDraggingOver(false);
-		
+		setIsDragging(false);
 	}
 
 	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault();
-		setIsDraggingOver(false);
-		
+		const droppedFiles = Array.from(event.dataTransfer.files);
+		setFiles(prevFiles => ([...prevFiles, ...droppedFiles]))
+		setIsDragging(false);
 	}
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
 		const filesToUpload = event.target.files;
-		setFiles(prevFiles => [...prevFiles, ...Array.from(filesToUpload)])
+		setFiles(prevFiles => [...prevFiles, ...Array.from(filesToUpload)]);
 	}
 
 	const handleRemove = (filename: string) => {
-		setFiles(prevFiles => prevFiles.filter(file => file.name !== filename))
+		setFiles(prevFiles => prevFiles.filter(file => file.name !== filename));
 	}
 
 	return (
 		<div className={styles.uploadImageContainer}>
 			<section
-				className={`${styles.dropZone} ${isDraggingOver && styles.dropZoneOver}`}
+				className={`${styles.dropZone} ${isDragging && styles.dropZoneOver}`}
 				onDragEnter={handleDragEnter}
-				onDragOver={handleDragAOver}
+				onDragOver={handleDragEnter}
 				onDragLeave={handleDragLeave}
 				onDrop={handleDrop}
 			>
@@ -91,7 +87,6 @@ const PreviewImage = ({ file, remove }: PreviewImageProps) => {
 		setUploadProgress(0);
 		const formData = new FormData();
 		formData.append('file', file);
-		
 		try {
 			const plantImageResponse = await plantService.uploadPlantImage({
 				plantId: Number.parseInt(plantId),
@@ -101,8 +96,7 @@ const PreviewImage = ({ file, remove }: PreviewImageProps) => {
 			});
 			setUploadStatus('success');
 			setUploadProgress(100);
-			console.log('Response', plantImageResponse)
-
+			console.log('Response', plantImageResponse);
 		} catch (error) {
 				setUploadStatus('error');
 				setUploadProgress(0);
@@ -132,10 +126,11 @@ const PreviewImage = ({ file, remove }: PreviewImageProps) => {
 			</article>
 			{uploadStatus === 'uploading'
 				? <UploadDetails progress={uploadProgress} />
-				: <p className={`${styles.textUploadStatus}
-						${uploadStatus === 'idle' ? styles.idle : uploadStatus === 'success' ? styles.success : styles.error}`}>
+				: uploadStatus !== 'idle' && (
+					<p className={`${styles.textUploadStatus} ${uploadStatus === 'success' ? styles.success : styles.error}`}>
 						{uploadStatus}
-					</p>}
+					</p>
+				)}
 		</div>
 	)
 }
@@ -170,77 +165,3 @@ const adjustSize = (size: number) => {
 		}
 	}
 }
-// type UploadStatus = 'idle' | 'error' | 'success' | 'uploading';
-
-// export const UploadImage = () => {
-// 	const { plantId }= useParams();
-// 	const [file, setFile] = useState<File | null>(null);
-// 	const [status, setStatus] = useState<UploadStatus>('idle');
-// 	const [uploadProgress, setUploadProgress] = useState(0);
-
-// 	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
-// 		const files: FileList = e.target.files;
-// 		if (files) {
-// 			setFile(files[0])
-// 		}
-// 	};
-
-//   const handleFileUpload = async () => {
-// 		console.log('plantId', plantId)
-//     if (file) {
-//       setStatus('uploading');
-//       setUploadProgress(0);
-//       const formData = new FormData();
-//       formData.append('file', file);
-
-//       try {
-//         const plantImageResponse = await plantService.uploadPlantImage({
-//           plantId: Number.parseInt(plantId),
-//           isSelected: false,
-//           formData,
-//           changePercentage: (percentage: number) => setUploadProgress(percentage),
-//         });
-//         setStatus('success');
-//         setUploadProgress(100);
-//         console.log('Response', plantImageResponse)
-
-//       } catch (e) {
-//         setStatus('error');
-//         setUploadProgress(0);
-//       }
-//     }
-//   };
-
-//   return (
-//     <section className={styles.uploadImageContainer}>
-//       <input type='file' accept='image/*' onChange={handleFileChange} />
-//       <div className={styles.informationContainer}>
-//         {file && (
-//           <article className={styles.imageInformation}>
-//             <p><b>File name: </b>{file.name}</p>
-//             <p><b>Size: </b>{(file.size / 1024).toFixed(2)} KB</p>
-//             <p><b>Type: </b>{file.type}</p>
-//           </article>
-//         )}
-//         {file && status !== 'uploading' && (
-//           <button className={styles.button} onClick={() => handleFileUpload()}>
-//             Upload File
-//           </button>
-//         )}
-//         {status === 'uploading' && (
-//           <div className={styles.progressContainer}>
-//             <div className={styles.progressBar}>
-//               <div
-//                 className={styles.progressFill}
-//                 style={{ width: `${uploadProgress}%` }}
-//               />
-//             </div>
-//             <p>{uploadProgress}%</p>
-//           </div>
-//         )}
-//       </div>
-//       {status === 'success' && <p>File uploaded successfully</p>}
-//       {status === 'error' && <p>Error in File uploading</p>}
-//     </section>
-//   );
-// }
